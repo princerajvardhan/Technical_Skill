342c530a140d19480923ccd070896f22b2111897
Prince raj Vardhan <134418662+princerajvardhan@users.noreply.github.com>
2026-02-03 10:58:51 +0530

    Add files via upload

---
+++ count.java
@@ -0,0 +1,20 @@
+ class count {
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 1, 3};
+        int n = arr.length;
+
+        int max = arr[0];
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        int time = 0;
+        for (int i = 0; i < n; i++) {
+            time += (max - arr[i]);
+        }
+
+        System.out.println(time); 
+    }
+}
